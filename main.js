68be64d93263b1fd6c292272f321fba8c03adc3b
Lydia karanja <lydiaa06karanja@gmail.com>
2026-07-09 18:57:04 +0300

    Add personal information and logging to main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+let myName = "Regina Gathoni";
+let myAge = 21;
+let isStudent = true;
+let favoriteColors = ["black", "purple", "burgundy"];
+const today = new Date("2026-07-09");
+
+console.log("My name is: " + myName);
+console.log("My age is: " + myAge);
+console.log("Am I a student? " + isStudent);
+console.log("My favorite colors are: " + favoriteColors);
+console.log("Today's date is: " + today.toDateString());
